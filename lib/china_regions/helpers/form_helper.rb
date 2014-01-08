e918fee83f141c3a2133fa4b2577bfd33edb1b50
Trey Springer <dspringer@enova.com>
2014-01-08 13:29:27 -0600

    changing the province dropdown deselects the city and district dropdowns instead of just the city one

--- lib/china_regions/helpers/form_helper.rb
+++ lib/china_regions/helpers/form_helper.rb
@@ -3,23 +3,22 @@
 module ChinaRegions
   module Helpers
     module FormHelper
-      
       def region_select(object, methods, options = {}, html_options = {})
         output = ''
 
         html_options[:class] ? 
           (html_options[:class].prepend('region_select ')) : 
             (html_options[:class] = 'region_select')
-            
+
         if Array === methods
           methods.each_with_index do |method, index|
             if region_klass = method.to_s.classify.safe_constantize
               choices = (index == 0 ? region_klass.scoped.collect {|p| [ p.name, p.id ] } : [])
               next_method = methods.at(index + 1)
-              
+
               set_options(method, options, region_klass)
               set_html_options(object, method, html_options, next_method)
-              
+
               output << select(object, "#{method.to_s}_id", choices, options = options, html_options = html_options)
             else
               raise "Method '#{method}' is not a vaild attribute of #{object}"
@@ -37,20 +36,20 @@ module ChinaRegions
 
           if region_klass = methods.to_s.classify.safe_constantize
             options[:prompt] = region_prompt(region_klass)
-            
+
             output << select(object, _methods, region_klass.scoped.collect {|p| [ p.name, p.id ] }, options = options, html_options = html_options)
           else
             raise "Method '#{method}' is not a vaild attribute of #{object}"
           end
         end
-        
+
         output << javascript_tag(js_output)
         output.html_safe
       end
-    
-    
+
+
       private
-      
+
       def set_options(method, options, region_klass)
         if respond_to?("#{method}_select_prompt")
           options[:prompt] = __send__("#{method}_select_prompt")
@@ -58,7 +57,7 @@ module ChinaRegions
           options[:prompt] = region_prompt(region_klass)
         end
       end
-      
+
       def set_html_options(object, method, html_options, next_region)
         html_options[:data] ? (html_options[:data][:region_klass] = "#{method.to_s}") : (html_options[:data] = { region_klass: "#{method.to_s}" })
         if next_region
@@ -68,34 +67,40 @@ module ChinaRegions
           html_options[:data].delete(:region_target_klass)
         end
       end
-        
+
       def region_prompt(region_klass)
         t('views.select', model: region_klass.model_name.human)
       end
-      
+
       def js_output
         %~
           $(function(){
             $('body').on('change', '.region_select', function(event) {
-              var self, targetDom;
-              self = $(event.currentTarget);
-              targetDom = $('#' + self.data('region-target'));
+              var changedObj, targetDom;
+              changedObj = $(event.currentTarget);
+              targetDom = $('#' + changedObj.data('region-target'));
               if (targetDom.size() > 0) {
-                $.getJSON('/china_regions/fetch_options', {klass: self.data('region-target-klass'), parent_klass: self.data('region-klass'), parent_id: self.val()}, function(data) {
+                $.getJSON('/china_regions/fetch_options', {klass: changedObj.data('region-target-klass'), parent_klass: changedObj.data('region-klass'), parent_id: changedObj.val()}, function(data) {
                   $('option[value!=""]', targetDom).remove();
                   $.each(data, function(index, value) {
                     targetDom.append("<option value='" + value.id + "'>" + value.name + "</option>");
                   });
                 })
+                // just clear out the second dropdown if it exists, they should start from the beginning
+                secondTargetDom = $('#' + targetDom.data('region-target'));
+                console.log(secondTargetDom)
+                if (secondTargetDom.size() > 0) {
+                  $('option[value!=""]', secondTargetDom).remove();
+                }
               }
             });
           });
         ~
       end
-  
+
     end
-    
-    
+
+
     module FormBuilder
       def region_select(methods, options = {}, html_options = {})
         @template.region_select(@object_name, methods, options = options, html_options = html_options)
