badff167b4d84fc5aba37c2725fa11f89e45e68f
Franco Reyes <freyes@enova.com>
2015-03-25 15:55:49 -0500

    Copy over js files in region generator
    Have the region generator copy over the javascript file to
    app/assets/javascript in the project in addition to the models.

--- lib/generators/china_regions/regions_generator.rb
+++ lib/generators/china_regions/regions_generator.rb
@@ -1,13 +1,17 @@
 # encoding: utf-8
 
 module ChinaRegions
-  class RegionsGenerator < Rails::Generators::NamedBase
+  class RegionsGenerator < Rails::Generators::Base
     source_root File.expand_path('../../../../app', __FILE__)
 
     def copy_models_file
       copy_file "models/province.rb", "app/models/province.rb"
       copy_file "models/city.rb",     "app/models/city.rb"
       copy_file "models/district.rb", "app/models/district.rb"
+    end
+
+    def copy_js_file
+      copy_file "assets/javascripts/region_select.js", "app/assets/javascripts/region_select.js"
     end
   end
 end
